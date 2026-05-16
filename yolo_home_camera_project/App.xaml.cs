374e3c1d450210aee693ab21450527d8a5c96808
choenamgyu9 <choenamgyu9@gmail.com>
2026-05-16 13:14:58 +0900

    초기 WPF 홈캠 프로젝트 추가

---
+++ yolo_home_camera_project/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace yolo_home_camera_project
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
