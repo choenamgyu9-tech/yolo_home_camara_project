49c1dc4775cdf764e039c1f23c57b2a25d1e4c25
HS_ <161850542+HSgithubHi@users.noreply.github.com>
2026-05-16 15:15:08 +0900

    addNewPage
    pages

---
+++ yolo_home_camera_project/video.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace yolo_home_camera_project
+{
+    /// <summary>
+    /// video.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class video : Page
+    {
+        public video()
+        {
+            InitializeComponent();
+        }
+    }
+}
